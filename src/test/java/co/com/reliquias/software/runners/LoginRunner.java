ac249b58776bfa60194a102d26daafdc3049b878
DiegoPinzon20 <diegopip62@gmail.com>
2023-03-13 12:27:55 -0500

    Se crea el ejecutor de la prueba automatizada.

---
+++ src/test/java/co/com/reliquias/software/runners/LoginRunner.java
@@ -0,0 +1,21 @@
+package co.com.reliquias.software.runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(CucumberWithSerenity.class) //-> Para trabajar con Cucumber y Serenity
+//@RunWith(SerenityRunner.class)
+//@RunWith(JUnit4.class)
+@CucumberOptions(
+        features = "src/test/resources/features/login/inicio_sesion.feature",
+        glue = "co.com.reliquias.software.definitions", //POM
+//        glue = "co.com.reliquias.software.stepdefinitions", //Screenplay
+        snippets = CucumberOptions.SnippetType.CAMELCASE, //navegarPaginaWeb
+        plugin = "pretty",
+        tags = "@InicioSesionExitoso"
+)
+public class LoginRunner {
+}
