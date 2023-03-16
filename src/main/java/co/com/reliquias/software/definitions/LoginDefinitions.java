02f1b79b51a59d8920e37251325d00d9b80a5640
DiegoPinzon20 <diegopip62@gmail.com>
2023-03-15 23:47:47 -0500

    SE AGREGAN LOS STEPS A LAS DEFINCIONES

--- src/main/java/co/com/reliquias/software/definitions/LoginDefinitions.java
+++ src/main/java/co/com/reliquias/software/definitions/LoginDefinitions.java
@@ -1,21 +1,30 @@
 package co.com.reliquias.software.definitions;
 
+import co.com.reliquias.software.steps.LoginSteps;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.es.Entonces;
+import net.thucydides.core.annotations.Steps;
 
 public class LoginDefinitions {
 
+    @Steps
+    private LoginSteps loginSteps;
+
     @Dado("que el usuario navega a la página de inicio de sesión")
     public void navegarPaginaInicioSesion() {
+        loginSteps.openLoginPage();
+        loginSteps.clearFieldsLogin();
     }
 
     @Cuando("ingresa las credenciales de acceso correctas")
     public void ingresaLasCredencialesDeAccesoCorrectas() {
+        loginSteps.enterCredentials("admin", "serenity");
+        loginSteps.accessSystem();
     }
 
     @Entonces("debería ver la página principal")
     public void deberiaVerPaginaPrincipal(){
-
+        loginSteps.validateSuccessfulLogin();
     }
 }
