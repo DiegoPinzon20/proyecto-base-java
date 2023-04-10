f18403f382a613fc876b22d2fdb2ce295ac9f06b
DiegoPinzon20 <diegopip62@gmail.com>
2023-04-09 22:36:26 -0500

    se agrega interacción general para cargar un archivo.

--- src/main/java/co/com/reliquias/software/pages/interacciones/GeneralInteraction.java
+++ src/main/java/co/com/reliquias/software/pages/interacciones/GeneralInteraction.java
@@ -1,9 +1,15 @@
 package co.com.reliquias.software.pages.interacciones;
 
+import co.com.reliquias.software.utilities.RobotUtility;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.pages.PageObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 
 public class GeneralInteraction extends PageObject {
 
@@ -19,6 +25,18 @@ public class GeneralInteraction extends PageObject {
     public void openPage(String url) {
         LOGGER.info("Se abre al URL:'{}'", url);
         getDriver().get(url);
+    }
+
+    public void uploadFile(WebElementFacade webElementFacade, String absolutePathFile) {
+        StringSelection stringSelection = new StringSelection(absolutePathFile);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+
+        webElementFacade.waitUntilClickable().click();
+        RobotUtility.wait(1500);
+        RobotUtility.pressManyKeys(KeyEvent.VK_CONTROL, KeyEvent.VK_V);
+        RobotUtility.releaseManyKeys(KeyEvent.VK_CONTROL, KeyEvent.VK_V);
+        RobotUtility.pressAndRelease(KeyEvent.VK_ENTER);
     }
 
 }
